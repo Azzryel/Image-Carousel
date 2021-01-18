e3b50620d1ed93b1f29dde9bdc5f1d0029593a04
Azzryel <AzryelShroud@gmail.com>
2021-01-18 18:01:01 +0100

    added buttons for image slide

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
+const leftBtn = document.querySelector("#left");
+const rightBtn = document.querySelector("#right");
+
 let myGallery = [];
+let currentSlide = 0;
 
 class Image {
     constructor(title, img) {
         this.title = title;
         this.img = img;
     }
-
 }
 
 //test images
-const image0 = new Image ("Image0", './images/photo0.jpeg');
+const image0 = new Image ("Image0", './images/photo1.jpeg');
 myGallery.push(image0);
-const image1 = new Image ("Image1", './images/photo1.jpeg');
+const image1 = new Image ("Image1", './images/photo2.jpeg');
 myGallery.push(image1);
-const image2 = new Image ("Image2", './images/photo2.jpeg');
+const image2 = new Image ("Image2", './images/photo3.JPG');
 myGallery.push(image2);
-const image3 = new Image ("Image3", './images/photo3.jpeg');
+const image3 = new Image ("Image3", './images/photo4.jpg');
 myGallery.push(image3);
 
 console.log(image1.title);
 console.log(myGallery[2]);
+console.log(myGallery[2].img);
 
-showImage(image2.img);
+slideImage(currentSlide);
 
 function showImage(img) {
     let image = document.querySelector("#image");
+    console.log(image);
     image.setAttribute("src", `${img}`);
+    console.log(image);
 }
 
+function slideImage(n) {
+    showImage(myGallery[n].img);
+}
+
+leftBtn.addEventListener("click", () => {
+    currentSlide -= 1;
+
+    if (currentSlide < 0) {
+        currentSlide =myGallery.length -1;
+    }
+    slideImage(currentSlide);
+});
+
+rightBtn.addEventListener("click", () => {
+    currentSlide += 1;
+
+    if (currentSlide > myGallery.length-1) {
+        currentSlide = 0;
+    }
+    slideImage(currentSlide);
+});
 
